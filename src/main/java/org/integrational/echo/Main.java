6ff28ea9ab2edecd717c9f9a6cba3c59930a95c9
Gerald Loeffler <gerald.loeffler@googlemail.com>
2022-07-22 10:59:44 +0200

    code and build

---
+++ src/main/java/org/integrational/echo/Main.java
@@ -0,0 +1,9 @@
+package org.integrational.echo;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Arrays.asList(args));
+    }
+}
